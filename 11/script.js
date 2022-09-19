b3267f90d92304ad25cb6e3627f518331a4c2010
Keks <keks@htmlacademy.ru>
2022-09-19 19:30:51 +0300

    :heavy_check_mark: Сборка #11

--- 11/script.js
+++ 11/script.js
@@ -1,12 +1,1 @@
-let navMain = document.querySelector('.nav');
-let navToggle = document.querySelector('.nav__toggle');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('nav--close')) {
-    navMain.classList.remove('nav--close');
-    navMain.classList.add('nav');
-  } else {
-    navMain.classList.add('nav--close');
-    navMain.classList.remove('nav');
-  }
-});
+let navMain=document.querySelector(".nav"),navToggle=document.querySelector(".nav__toggle");navToggle.addEventListener("click",(function(){navMain.classList.contains("nav--close")?(navMain.classList.remove("nav--close"),navMain.classList.add("nav")):(navMain.classList.add("nav--close"),navMain.classList.remove("nav"))}));
